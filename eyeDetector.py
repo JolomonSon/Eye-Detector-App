d8308192f66a28b899698c0d90f9233b22f99201
John Solomon <jolomonson@gmail.com>
2021-04-29 02:46:14 +0100

    Starting off with face detection

---
+++ eyeDetector.py
@@ -0,0 +1,28 @@
+import cv2
+from random import randrange
+# Real Time Image
+video = cv2.VideoCapture(0)
+# Eye Classifier
+faceClassifier = cv2.CascadeClassifier('frontalFaceDetector.xml')
+eyeClassifier = cv2.CascadeClassifier('haarcascade_eye.xml')
+while True:
+    readSuccessful, frame = video.read()
+    if not readSuccessful:
+        break
+    # Converting Image to Gray Scale
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Getting Frame Coordinates
+    faceCoordinates = faceClassifier.detectMultiScale(grayFrame)
+    eyesCoordinates = eyeClassifier.detectMultiScale(grayFrame)
+    print('Face Coordinates - ',faceCoordinates)
+    print('Eyes Coordinates - ',eyesCoordinates)
+    #Drawing Rectangles
+    face = [cv2.rectangle(frame, (x,y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2) for (x,y,w,h) in faceCoordinates]
+    eyes = [cv2.rectangle(frame, (x,y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)), 2) for (x,y,w,h) in eyesCoordinates]
+    cv2.imshow('Eye Detection', frame)
+    key = cv2.waitKey(1)
+    # Stop if Q is pressed
+    if key==81 or key==113:
+        break
+video.release()
+print('Code Completed')
